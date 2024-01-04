9f9c9db0211823d15cb09749fb36eab359650753
Adam Carter <adam.clayton.carter@gmail.com>
2024-01-04 18:33:11 -0500

    Improve class design by making bean instances immutable

--- src/main/java/services/CommentService.java
+++ src/main/java/services/CommentService.java
@@ -1,14 +1,16 @@
 package services;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import repositories.CommentRepository;
 
 @Service
 public class CommentService {
 
-    @Autowired
-    private CommentRepository commentRepository;
+    private final CommentRepository commentRepository;
+
+    public CommentService(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
 
     public CommentRepository getCommentRepository() {
         return commentRepository;
--- src/main/java/services/UserService.java
+++ src/main/java/services/UserService.java
@@ -1,14 +1,16 @@
 package services;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import repositories.CommentRepository;
 
 @Service
 public class UserService {
 
-    @Autowired
-    private CommentRepository commentRepository;
+    private final CommentRepository commentRepository;
+
+    public UserService(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
 
     public CommentRepository getCommentRepository() {
         return commentRepository;
