496be831e66ff0e59e17f622be3277ab872636a9
Adam Carter <adam.clayton.carter@gmail.com>
2024-01-04 19:43:48 -0500

    Declare prototype-scoped beans with stereotype annotations

--- src/main/java/config/ProjectConfig.java
+++ src/main/java/config/ProjectConfig.java
@@ -2,11 +2,13 @@ package config;
 
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 import services.CommentService;
 
-@Configuration
+@Configuration()
+@ComponentScan(basePackages = {"services", "repositories"})
 public class ProjectConfig {
 
     @Bean
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,16 +3,17 @@ package main;
 import config.ProjectConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import services.CommentService;
+import services.UserService;
 
 public class Main {
     public static void main(String[] args) {
         var c = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
-        var cs1 = c.getBean("commentService", CommentService.class);
-        var cs2 = c.getBean("commentService", CommentService.class);
+        var s1 = c.getBean(CommentService.class);
+        var s2 = c.getBean(UserService.class);
 
-        boolean b1 = cs1 == cs2;
+        boolean b = s1.getCommentRepository() == s2.getCommentRepository();
 
-        System.out.println(b1);
+        System.out.println(b);
     }
 }
